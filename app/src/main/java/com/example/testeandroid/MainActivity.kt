498ae623f31d72453d40d72215acad4b1cca9b89
Henrique Sant'Anna <hsantanna@gmail.com>
2023-11-13 09:44:50 -0300

    codigo inicial, intent para segunda activity

---
+++ app/src/main/java/com/example/testeandroid/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.testeandroid
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val context : Context get() = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun exibirTexto(view: View){
+        val editText = findViewById<EditText>(R.id.editTextText)
+        // Toast.makeText(this, "Chamou: ${editText.text.toString()}", Toast.LENGTH_SHORT).show()
+
+        val intento = Intent(context, segundaActivity::class.java)
+
+        val parametros = Bundle()
+        parametros.putString("msg", editText.text.toString())
+        intento.putExtras(parametros)
+
+        startActivity(intento)
+
+    }
+}
---
+++ app/src/main/java/com/example/testeandroid/segundaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testeandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class segundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+    }
+}
