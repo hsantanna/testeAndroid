fa8086adcd79c70d05937a4b2401a128729b72ec
Henrique Sant'Anna <hsantanna@gmail.com>
2023-11-13 10:50:27 -0300

    segundaActivity recebe e exibe dados

--- app/src/main/java/com/example/testeandroid/segundaActivity.kt
+++ app/src/main/java/com/example/testeandroid/segundaActivity.kt
@@ -2,10 +2,18 @@ package com.example.testeandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class segundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val params = intent.extras
+        val mensagem = params?.getString("msg")
+
+        textView.setText("Mensagem recebida: $mensagem")
     }
 }
